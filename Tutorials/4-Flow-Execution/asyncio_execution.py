cb74f452cb5ab2e7368ca969b5cb950c89c1b756
Peeush Agarwal <peeush.agarwal@privacera.com>
2022-06-03 09:31:47 +0530

    Add asyncio execution demo script

---
+++ Tutorials/4-Flow-Execution/asyncio_execution.py
@@ -0,0 +1,19 @@
+import asyncio
+from prefect import flow, task
+
+@task
+async def print_values(values):
+    for value in values:
+        await asyncio.sleep(1)
+        print(value, end=" ")
+
+@flow
+async def async_flow():
+    await print_values([1, 2]) # Runs immediately
+    coros = [print_values("abcd"), print_values("6789")]
+
+    # asynchronously gather the tasks
+    await asyncio.gather(*coros)
+
+if __name__ == "__main__":
+    asyncio.run(async_flow())
