dead8e757e1a9d9315952553dafc4dd145617c3a
Peeush Agarwal <peeush.agarwal@privacera.com>
2022-06-03 09:22:29 +0530

    Add python script for Flow execution tutorial

---
+++ Tutorials/4-Flow-Execution/example.py
@@ -0,0 +1,18 @@
+import time
+
+from prefect import task, flow
+from prefect.task_runners import DaskTaskRunner
+
+@task
+def print_values(values):
+    for value in values:
+        time.sleep(0.5)
+        print(value, end="\r")
+
+@flow(task_runner=DaskTaskRunner())
+def parallel_flow():
+    print_values(["AAAA"] * 15)
+    print_values(["BBBB"] * 10)
+
+if __name__ == "__main__":
+    parallel_flow()
