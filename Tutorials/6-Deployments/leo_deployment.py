ce347cd9bdd8e4255adc72b6b63e5d6ca7093137
Peeush Agarwal <peeush.agarwal@privacera.com>
2022-06-04 13:16:44 +0530

    Update deployment with new deployment spec

--- Tutorials/6-Deployments/leo_deployment.py
+++ Tutorials/6-Deployments/leo_deployment.py
@@ -1,8 +1,17 @@
 from prefect.deployments import DeploymentSpec
+from prefect.flow_runners import SubprocessFlowRunner
 
 DeploymentSpec(
     name="leonardo-deployment",
     flow_location="./leo_flow.py",
     tags=["tutorial", "test"],
     parameters={'name':'Leo'}
+)
+
+DeploymentSpec(
+    name="marvin-deployment",
+    flow_location="./leo_flow.py",
+    tags=["tutorial", "dev"],
+    flow_runner=SubprocessFlowRunner(),
+    parameters={'name':'Marvin'}
 )
